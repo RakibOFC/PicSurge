a20c66ff2d97d26fffd42dfdf9626566891dfab3
RakibOFC <rakibull2000@gmail.com>
2023-05-26 12:09:53 +0600

    Simplified the whole code

--- app/src/main/java/com/rakibofc/picsurge/viewmodels/MainViewModel.java
+++ app/src/main/java/com/rakibofc/picsurge/viewmodels/MainViewModel.java
@@ -3,19 +3,13 @@ package com.rakibofc.picsurge.viewmodels;
 import android.app.Application;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Target;
-
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainViewModel extends AndroidViewModel {
@@ -26,6 +20,7 @@ public class MainViewModel extends AndroidViewModel {
         super(application);
 
         liveImage = new MutableLiveData<>();
+        loadData();
     }
 
     public LiveData<Bitmap> getImage() {
@@ -34,39 +29,17 @@ public class MainViewModel extends AndroidViewModel {
 
     public void loadData() {
 
-        /*String imageUrl = "https://loremflickr.com/320/240";
+        String imageUrl = "https://loremflickr.com/320/240";
 
         new Thread(() -> {
 
             try {
                 URL url = new URL(imageUrl);
-                liveImage.setValue(BitmapFactory.decodeStream(url.openConnection().getInputStream()));
+                liveImage.postValue(BitmapFactory.decodeStream(url.openConnection().getInputStream()));
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            Log.e("Info", "Bitmap bitmap");
-
-        }).start();*/
-
-        }
-
-        /*Picasso.get().load(imageUrl).into(new Target() {
-            @Override
-            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                Log.e("Info", "Bitmap bitmap");
-                liveImage.setValue(bitmap);
-            }
-
-            @Override
-            public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-                Log.e("Info", "Image Failed");
-            }
-
-            @Override
-            public void onPrepareLoad(Drawable placeHolderDrawable) {
-                Log.e("Info", "Set Image");
-            }
-        });*/
-
+        }).start();
+    }
 }
--- app/src/main/java/com/rakibofc/picsurge/views/MainActivity.java
+++ app/src/main/java/com/rakibofc/picsurge/views/MainActivity.java
@@ -4,16 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.IntentFilter;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.StrictMode;
 
 import com.rakibofc.picsurge.databinding.ActivityMainBinding;
 import com.rakibofc.picsurge.receivers.ConnectionReceiver;
 import com.rakibofc.picsurge.viewmodels.MainViewModel;
-
-import java.io.IOException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,46 +37,14 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction(CONNECTIVITY_ACTION);
 
         // Get and set image from ViewModel
-        viewModel.getImage().observe(this, binding.imageBtn::setImageBitmap);
+        viewModel.getImage().observe(this, binding.imageView::setImageBitmap);
 
-        /*
-        * viewModel.getImage().observe(this, new Observer<Drawable>() {
-            @Override
-            public void onChanged(Drawable drawable) {
-                binding.imageBtn.setImageDrawable(drawable);
-            }
-        });
-        * */
+        viewModel.getImage().observe(this, binding.imageView::setImageBitmap);
 
-        binding.imageBtn.setOnClickListener(v -> {
+        binding.imageView.setOnClickListener(v -> {
 
-            String imageUrl = "https://loremflickr.com/320/240";
-
-            new Thread(() -> {
-                try {
-                    synchronized (this) {
-                        wait(500);
-
-                        runOnUiThread(() -> {
-
-                            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                            StrictMode.setThreadPolicy(policy);
-
-                            try {
-                                URL url = new URL(imageUrl);
-
-                                binding.imageBtn.setImageBitmap(
-                                        BitmapFactory.decodeStream(url.openConnection().getInputStream()));
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        });
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            // Click listener to load data from ViewModel
+            viewModel.loadData();
         });
     }
 
